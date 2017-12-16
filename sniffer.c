67f3a2f7654bd0a8d1d44cf0cc532024b018d0a8
Hokkaitao <tsthght@yeah.net>
2017-12-16 20:23:54 +0800

    fix compile err

--- sniffer.c
+++ sniffer.c
@@ -666,7 +666,7 @@ void getCurrentTime(char *dst) {
 	gettimeofday(&tv, &tz);
 	p = localtime(&tv.tv_sec);
 	if(p == NULL) {
-		return
+		return;
 	}
 	sprintf(dst, "%d-%d-%d %d:%d:%d.%ld", 1900+p->tm_year, 1+p->tm_mon, p->tm_mday, p->tm_hour, p->tm_min, p->tm_sec, tv.tv_usec);
 }
